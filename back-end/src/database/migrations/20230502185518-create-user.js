44d2723813555481e5908d1239167a923ca451be
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-05-02 16:07:30 -0300

    fix: corrigido o nome do arquivo de migration create-user e inserida função up

---
+++ back-end/src/database/migrations/20230502185518-create-user.js
@@ -0,0 +1,33 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'display_name',
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  async down(queryInterface, _Sequelize) {
+    return queryInterface.dropTable('users');
+  },
+};

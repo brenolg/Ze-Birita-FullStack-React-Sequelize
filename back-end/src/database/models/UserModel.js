336607c17bd978fc7b95f5479fb7b27ce4277c13
Lígia Bicalho <ligiabicalho@gmail.com>
2023-05-02 17:14:07 -0300

    feat: adiciona Users seed

--- back-end/src/database/models/UserModel.js
+++ back-end/src/database/models/UserModel.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const {  Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class UserModel extends Model {
     static associate(models) {
@@ -9,13 +7,28 @@ module.exports = (sequelize, DataTypes) => {
   }
   UserModel.init({
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING,
+    name:  {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(32),
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
   }, {
     sequelize,
     modelName: 'users',
+    timestamps: false,
+    underscored: true
   });
   return UserModel;
 };

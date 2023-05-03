1c8c450357d627eb1d238940db358ad38d4f6e7d
brenolg <brenolg@yahoo.com.br>
2023-05-03 15:49:11 -0300

    chore: addicionando async errors + joi

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -13,21 +13,13 @@ class UserController extends AbstractController {
   }
 
   async login() {
-    try {
       const newObj = await this.userService.login(this.req.body);
       return this.res.status(StatusCodes.OK).json(newObj);
-    } catch (error) {
-      this.next(error);
-    }
   }
 
   async register() {
-    try {
       const newObj = await this.userService.register(this.req.body);
       return this.res.status(StatusCodes.CREATED).json(newObj);
-    } catch (error) {
-      this.next(error);
-    }
   }
 }
 

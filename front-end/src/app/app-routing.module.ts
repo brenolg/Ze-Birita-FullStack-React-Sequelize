20a988b3fde6979d551a342403d433986ab18203
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-05-03 15:40:20 -0300

    chore: criado componente para as páginas de pedidos

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import { OrdersComponent } from './pages/orders/orders.component';
 import { ProductsComponent } from './pages/products/products.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: LoginComponent },
+  { path: ':role/orders', component: OrdersComponent },
   { path: ':role/products', component: ProductsComponent },
 ]
 

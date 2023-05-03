e5e30d39dc9a30888c3d8e84aa8b465912112a5b
Lígia Bicalho <ligiabicalho@gmail.com>
2023-05-02 22:42:43 -0300

    feat: adiciona Login Router

---
+++ back-end/src/routers/LoginRouter.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import 'express-async-errors';
+
+
+const router = Router();
+
+//registro
+router.post('/', (req, res) => {});
+
+//login
+router.get('/', (req, res) => {});
+
+
+export default router;

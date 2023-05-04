3a745ac72d094cc2d1a7b4a4013635d29d5dec29
brenolg <brenolg@yahoo.com.br>
2023-05-03 22:32:21 -0300

    feat: model products

--- back-end/src/database/models/User.js
+++ back-end/src/database/models/User.js
@@ -1,22 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+
     name: { type: DataTypes.STRING(100), allowNull: false },
+
     email: {
       type: DataTypes.STRING(100),
       allowNull: false,
       unique: true,
     },
+
     password: { type: DataTypes.STRING(32), allowNull: false },
     role: { type: DataTypes.STRING(20), allowNull: false , defaultValue: 'customer'},
+    
   }, {
     tableName: 'users',
     timestamps: false,
     underscored: true,
   });
 
-  // UserModel.associate = (models) => {
-  //   UserModel.hasMany()
-  // };
   return User;
 };

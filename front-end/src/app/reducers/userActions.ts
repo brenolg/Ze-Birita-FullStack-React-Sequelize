1a26ad9b9b299c3b04994cc922efdcc55102c1eb
Lígia Bicalho <ligiabicalho@gmail.com>
2023-05-12 18:20:39 -0300

    iniciando projeto React

--- front-end/src/app/reducers/userActions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { IState } from '../interfaces/user.interface';
-
-export const getLoginInfo = createAction('GET_LOGIN_INFO', props<IState>());
--- front-end/src/app/reducers/userSelectors.ts
+++
@@ -1,9 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { IState } from '../interfaces/user.interface';
-
-export const selectUser = createSelector(
-  createFeatureSelector('user'),
-  (state: IState) => {
-    return state.user?.name;
-  }
-)

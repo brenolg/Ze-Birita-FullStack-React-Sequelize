000d21fa1e4bbda75e4f10c312683ce74ad11038
brenolg <brenolg@yahoo.com.br>
2023-05-13 19:19:59 -0300

    chore: inicio da logica para um boleano se o usuario logou

--- front-end/src/context/Provider.js
+++ front-end/src/context/Provider.js
@@ -1,10 +1,28 @@
 import PropTypes from 'prop-types';
 import React, { useMemo, useState } from 'react';
 import Context from './Context';
+import DataHandler from '../utils/DataHandler';
 
 export default function Provider({ children }) {
   const [logIn, setLogIn] = useState(false);
   const [userData, setUserData] = useState(0);
+
+  const userHasLogin = async () => {
+    const userLogin = await DataHandler.get('user').user || null;
+    let hasLogin = false;
+    if (userLogin) {
+      hasLogin = true;
+    }
+    if (userData === null) {
+      hasLogin = false;
+    }
+    setLogIn(hasLogin);
+    return hasLogin;
+  };
+
+  useEffect(() => {
+    setLogIn(userHasLogin());
+  }, [setUserData, setLogIn]);
 
   const value = useMemo(
     () => ({

46bfd8e504f412505cdfea639b2227bfc97e05d8
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-05-16 15:26:45 -0300

    Feat: Adiciona multer na rota de produtos

--- back-end/src/routers/ProductRouter.js
+++ back-end/src/routers/ProductRouter.js
@@ -3,25 +3,34 @@ const ProductController = require('../controllers/ProductController');
 const ProductService = require('../services/ProductService');
 const UserHandler = require('../middlewares/UserMiddlewares');
 const role = require('../utils/rolesList');
+const upload = require('../utils/upload');
 
 const router = Router();
 const productService = new ProductService();
 
-router.get('/', (req, res, next) => new ProductController(productService, req, res, next)
-.getAll());
+router.get(
+  '/', 
+  (req, res, next) => new ProductController(productService, req, res, next)
+  .getAll(),
+);
 
-router.get('/:id', (req, res, next) => new ProductController(productService, req, res, next)
-.getById());
+router.get(
+  '/:id', 
+  (req, res, next) => new ProductController(productService, req, res, next)
+  .getById(),
+);
 
-router.post('/', (req, res, next) => new ProductController(productService, req, res, next)
-.create());
+router.post(
+  '/register', 
+  upload.single('image'), 
+  (req, res, next) => new ProductController(productService, req, res, next)
+  .create(),
+);
 
 router.delete(
 '/:id', 
-
 UserHandler.defaultAccess,
 UserHandler.roleAccess(role.ADMIN),
-
 (req, res, next) => new ProductController(productService, req, res, next)
 .remove(),
 );
